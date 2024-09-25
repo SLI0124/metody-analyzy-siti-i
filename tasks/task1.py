302aba116dd77156518a69ec0b7805c66884786a
Vojtech Sliva <sli0124@vsb.cz>
2024-09-25 23:35:20 +0200

    T1: min, max, avr degree with index

--- tasks/task1.py
+++ tasks/task1.py
@@ -40,6 +40,35 @@ def get_adjacency_list(data):
     return adjacency_list
 
 
+def calculate_max_degree(adjacency_list):
+    max_degree = 0
+    node_index = 0
+    for idx, row in enumerate(adjacency_list):
+        degree = len(row)
+        if degree > max_degree:
+            max_degree = degree
+            node_index = idx
+    return max_degree, node_index
+
+
+def calculate_min_degree(adjacency_list):
+    min_degree = len(adjacency_list[0])
+    node_index = 0
+    for idx, row in enumerate(adjacency_list):
+        degree = len(row)
+        if degree < min_degree:
+            min_degree = degree
+            node_index = idx
+    return min_degree, node_index
+
+
+def calculate_avg_degree(adjacency_list):
+    total_degree = 0
+    for idx, row in enumerate(adjacency_list):
+        total_degree += len(row)
+    return total_degree / len(adjacency_list)
+
+
 def main():
     data = read_csv("../datasets/KarateClub.csv")
     formatted_data = format_data(data)
@@ -53,6 +82,15 @@ def main():
     adjacency_list = get_adjacency_list(formatted_data)
     for idx, row in enumerate(adjacency_list):
         print(f"{idx + 1}: {row}")
+
+    max_degree, max_node_index = calculate_max_degree(adjacency_list)
+    print(f"\nMax degree: {max_degree} with index: {max_node_index + 1}\n")
+
+    min_degree, min_node_index = calculate_min_degree(adjacency_list)
+    print(f"Min degree: {min_degree} with index: {min_node_index + 1}\n")
+
+    avg_degree = calculate_avg_degree(adjacency_list)
+    print(f"Avg degree: {avg_degree:.2f} ({avg_degree})\n")
 
 
 if __name__ == "__main__":
