f87fa46afdf67e3290872476c88a28207c33f1c8
Vojtech Sliva <sli0124@vsb.cz>
2024-11-19 08:48:57 +0100

    feat(task7): generating csvs with node properties and edges, plot with degree distribution and overall summary

--- tasks/task7.py
+++ tasks/task7.py
@@ -3,11 +3,17 @@ import random
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 
-from task6 import save_edges_to_csv, save_nodes_to_csv, save_properties_to_csv, calculate_path_distance, \
-    calculate_diameter_and_radius, count_weakly_connected_components, save_plot, \
-    calculate_average_clustering_coefficient, has_graph_loops, has_graph_multi_edges
+from task6 import save_edges_to_csv, save_nodes_to_csv, save_properties_to_csv
 
 STARTING_NODES_COUNT_RANGE = (5, 10)
+
+
+def has_graph_loops(G: nx.Graph) -> bool:
+    return any(u == v for u, v in G.edges)
+
+
+def has_graph_multi_edges(G: nx.Graph) -> bool:
+    return any(G.has_edge(u, v) for u, v in G.edges if G.has_edge(v, u))
 
 
 def generate_connected_graph(size: int) -> nx.Graph:
@@ -74,18 +80,64 @@ def create_barabasi_albert_graph(G: nx.Graph, m: int, n: int) -> nx.Graph:
     return G
 
 
-def calculate_graph_properties(G: nx.Graph) -> dict:
-    pass
+def calculate_graph_properties(G: nx.Graph, initial_node_count: int, initial_edge_count: int) -> list:
+    """
+    Calculate graph properties for a given graph: initial number of nodes, initial number of edges, number of nodes,
+    number of edges, average degree, average distance, average clustering coefficient, longest path, shortest path,
+    number of connected components, largest component size, average component size.
+    :param G: networkx graph
+    :param initial_node_count: initial number of nodes in the graph before Barabasi-Albert model
+    :param initial_edge_count: initial number of edges in the graph before Barabasi-Albert model
+    :return: list of graph properties
+    """
+    number_of_nodes = G.number_of_nodes()
+    number_of_edges = G.number_of_edges()
+    average_degree = sum(dict(G.degree()).values()) / number_of_nodes
+    average_distance = nx.average_shortest_path_length(G)
+    average_clustering_coefficient = nx.average_clustering(G)
+    longest_path = nx.diameter(G)
+    shortest_path = nx.radius(G)
+    number_of_connected_components = nx.number_connected_components(G)
+    largest_component_size = max(len(c) for c in nx.connected_components(G))
+    average_component_size = number_of_nodes / number_of_connected_components
+
+    return [
+        initial_node_count, initial_edge_count, number_of_nodes, number_of_edges, average_degree, average_distance,
+        average_clustering_coefficient, longest_path, shortest_path, number_of_connected_components,
+        largest_component_size, average_component_size
+    ]
+
+
+def plot_degree_distribution(G: nx.Graph, m: int, file_name: str, directory_prefix: str):
+    """Plot the bar plot as histogram"""
+    degrees_distribution = dict(G.degree())
+
+    # Apply a more visually appealing style
+    plt.figure(figsize=(10, 6))  # Set a larger figure size
+
+    # Create a bar plot for the degree distribution
+    plt.bar(degrees_distribution.keys(), degrees_distribution.values(), color='C0', width=1.0)
+
+    # Enhance plot labels and title
+    plt.xlabel("Node Degree", fontsize=14)
+    plt.ylabel("Number of Nodes", fontsize=14)
+    plt.title(f"Degree Distribution of Barabási-Albert Graph (m={m})", fontsize=16)
+
+    # Add grid lines for better readability
+    plt.grid(visible=True, which='both', linestyle='--', linewidth=0.5, alpha=0.7)
+
+    # Save the plot to the specified file
+    plt.savefig(f"{directory_prefix}{file_name}.png")
+    plt.close()
 
 
 def main():
     m_sizes = [2, 3]
     target_nodes_count = 550
     headers = [
-        "Number of nodes", "Number of edges", "Average degree",
-        "Average distance", "Average clustering coefficient", "The longest path",
-        "The shortest path", "Number of connected components", "Largest component size",
-        "Average component size"
+        "Initial number of nodes", "Initial number of edges", "Number of nodes", "Number of edges", "Average degree",
+        "Average distance", "Average clustering coefficient", "Longest path", "Shortest path",
+        "Number of connected components", "Largest component size", "Average component size"
     ]
     directory_prefix = "../results/task7/"
     data = []
@@ -93,19 +145,26 @@ def main():
     for m in m_sizes:
         initial_node_count = random.randint(*STARTING_NODES_COUNT_RANGE)
         G = generate_connected_graph(initial_node_count)
+        initial_edge_count = G.number_of_edges()
         BA_G = create_barabasi_albert_graph(G, m, target_nodes_count)
         print(f"Generated Barabasi-Albert graph with m={m}, n={target_nodes_count}")
         print(f"Number of nodes: {BA_G.number_of_nodes()}")
-        if has_graph_loops(BA_G) or has_graph_multi_edges(BA_G):
-            print("Graph has loops or multi-edges")
-            continue
+        print(f"Number of edges: {BA_G.number_of_edges()}")
+        print(f"Number of initial nodes: {initial_node_count}")
+        print(f"Number of initial edges: {initial_edge_count}")
+        if not has_graph_loops(BA_G) or not has_graph_multi_edges(BA_G):
+            print("Graph has loops or multi-edges. Saving graph to CSV files...")
+            save_edges_to_csv(list(BA_G.edges), f"BA_{m}", directory_prefix)
         else:
             print("Graph has loops or multi-edges")
-            save_edges_to_csv(list(BA_G.edges), f"BA_{m}", directory_prefix)
-        # data.append(calculate_graph_properties(BA_G))
+            continue
+        print()
+        graph_properties = calculate_graph_properties(BA_G, initial_node_count, initial_edge_count)
+        data.append(graph_properties)
+        plot_degree_distribution(BA_G, m, f"BA_{m}_degree_distribution", directory_prefix)
 
-    # print(tabulate(data, headers=headers, tablefmt="grid"))
-    # save_properties_to_csv(data, "graph_properties", headers, directory_prefix)
+    print(tabulate(data, headers=headers, tablefmt="grid"))
+    save_properties_to_csv(data, "graph_properties", headers, directory_prefix)
 
 
 if __name__ == '__main__':
